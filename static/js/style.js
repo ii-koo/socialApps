eb1ee661259a0bc6deeedb30a1672f0a45deb834
ii-san <mhdihsan24@gmail.com>
2022-11-04 15:49:14 +0700

    add improvement part 2

---
+++ static/js/style.js
@@ -0,0 +1,19 @@
+function commentReplyToggle(parent_id){
+    const row = document.getElementById(parent_id);
+
+    if(row.classList.contains('d-none')){
+        row.classList.remove('d-none');
+    }else{
+        row.classList.add('d-none');
+    }
+}
+
+function commentReplyEditToggle(parent_id){
+    const row = document.getElementById(parent_id);
+
+    if(row.classList.contains('d-none')){
+        row.classList.remove('d-none');
+    }else{
+        row.classList.add('d-none');
+    }
+}
