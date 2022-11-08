5230530566ce0eb2b6ddbc72e218cf1f4f7c85b6
ii-san <mhdihsan24@gmail.com>
2022-11-08 07:08:56 +0700

    uploads image

--- social/forms.py
+++ social/forms.py
@@ -12,9 +12,11 @@ class PostForm(forms.ModelForm):
         })
     )
 
+    image = forms.ImageField(required=False)
+
     class Meta:
         model = Post
-        fields = ['body']
+        fields = ['body', 'image']
 
 
 class CommentForm(forms.ModelForm):
